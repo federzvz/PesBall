c183dae0d68a408f023f592a0381b5127b45b7be
federzvz <85709865+federzvz@users.noreply.github.com>
2022-09-26 17:28:04 -0300

    V0.0.2
    - A user interface and the scene corresponding to the Lobby were designed.
    - Creation of Scripts for ObjectController, NetworkManager, PlayerListItem, LobbyController and PlayerObjectController.
    - Adjusted the relationship between scene objects and script fields in the editor.

--- PesBall/Assets/SteamLobby.cs
+++ PesBall/Assets/SteamLobby.cs
@@ -5,8 +5,10 @@ using Mirror;
 using Steamworks;
 using UnityEngine.UI;
 
-public class SteamLobby : MonoBehaviour
-{
+public class SteamLobby : MonoBehaviour{
+
+    public static SteamLobby Instance;
+
     //Callbacks
     protected Callback<LobbyCreated_t> LobbyCreated;
     protected Callback<GameLobbyJoinRequested_t> joinRequest;
@@ -17,12 +19,10 @@ public class SteamLobby : MonoBehaviour
     private const string HostAddressKey = "HostAddress";
     private CustomNetworkManager manager;
 
-    //Gameobject
-    public GameObject HostButton;
-    public Text LobbyNameText;
 
     private void Start() {
         if (!SteamManager.Initialized) { return; }
+        if (Instance == null) { Instance = this; } 
 
         manager = GetComponent<CustomNetworkManager>();
 
@@ -54,10 +54,7 @@ public class SteamLobby : MonoBehaviour
     private void OnLobbyEntered(LobbyEnter_t callback)
     {
         //Everyone
-        HostButton.SetActive(false);
         CurrentLobbyID = callback.m_ulSteamIDLobby;
-        LobbyNameText.gameObject.SetActive(true);
-        LobbyNameText.text = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "name");
 
         //Clients
         if (NetworkServer.active){return;}
